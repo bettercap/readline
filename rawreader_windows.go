200cc6780de9f7f151fed347dab0ce051d39abf1
evilsocket <evilsocket@gmail.com>
2018-02-08 09:01:10 +0100

    balls

--- rawreader_windows.go
+++ rawreader_windows.go
@@ -60,9 +60,8 @@ next:
 	*/
 	_, _, err = kernel.ReadConsoleInputW.Call(stdin, uintptr(unsafe.Pointer(ir)), uintptr(1), uintptr(unsafe.Pointer(pread)))
 	if err != nil {
-		// fmt.Printf("\nReadConsoleInputW: err=%v ir=%v read=%v pread=%v\n", err, ir, read, pread)
-		goto next
-		// return 0, err
+		fmt.Printf("\nReadConsoleInputW: err=%v ir=%v read=%v pread=%v\n", err, ir, read, pread)
+		return 0, err
 	}
 
 	if ir.EventType != EVENT_KEY {
